35b387a32294f921776ab1e0120421dc6dbaefd9
Felipe Pérez <felipepepe@gmail.com>
2020-12-10 19:06:15 +0100

    fix test problems

--- src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
+++ src/common/components/confirmation-dialog/confirmation-dialog.component.spec.tsx
@@ -1,99 +1,127 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
+import { Typography } from '@material-ui/core';
 
-describe('Confirmation Dialog Component specs', () => {
-  it('should verified the title is in the confirmation dialog', () => {
+describe('Confirmation Dialog Components specs', () => {
+  it('Should display confirmation dialog components click on Accept', () => {
     // Arrange
-    const isOpen = true;
-    const title = 'test title';
-    const labels = {
-      closeButton: 'test Close',
-      acceptButton: 'test Accept',
+    const props = {
+      isOpen: true,
+      title: 'test title',
+      labels: {
+        closeButton: 'test Close',
+        acceptButton: 'test Accept',
+      },
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
     };
 
-    const onAccept = () => {
-      console.log('onAccept pressed');
-    };
+    const itemName = 'Hello Testing World';
 
-    const onClose = () => {
-      console.log('onClose pressed');
-    };
     // Act
-    const { getByText } = render(
-      <ConfirmationDialogComponent
-        isOpen={isOpen}
-        title={title}
-        labels={labels}
-        onAccept={onAccept}
-        onClose={onClose}
-      />
+    render(
+      <ConfirmationDialogComponent {...props}>
+        <Typography variant="body1">{itemName}</Typography>
+      </ConfirmationDialogComponent>
     );
 
+    const element = screen.getByRole('button', {
+      name: props.labels.acceptButton,
+    });
+
+    userEvent.click(element);
+
     // Assert
-    const element = getByText(title);
-    expect(element).not.toBeNull();
+    expect(props.onAccept).toHaveBeenCalled();
+  });
+  it('Should display confirmation dialog components click on Accept', () => {
+    // Arrange
+    const props = {
+      isOpen: true,
+      title: 'test title',
+      labels: {
+        closeButton: 'test Close',
+        acceptButton: 'test Accept',
+      },
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+    };
+
+    const itemName = 'Hello Testing World';
+
+    // Act
+    render(
+      <ConfirmationDialogComponent {...props}>
+        <Typography variant="body1">{itemName}</Typography>
+      </ConfirmationDialogComponent>
+    );
+
+    const element = screen.getByRole('button', {
+      name: props.labels.acceptButton,
+    });
+
+    userEvent.click(element);
+
+    // Assert
+    expect(props.onAccept).toHaveBeenCalled();
   });
 
-  it('should display confirmation dialog using snapshot testing', () => {
+  it('Should display confirmation dialog components', () => {
     // Arrange
-    const isOpen = true;
-    const title = 'test title';
-    const labels = {
-      closeButton: 'test Close',
-      acceptButton: 'test Accept',
+    const props = {
+      isOpen: true,
+      title: 'test title',
+      labels: {
+        closeButton: 'test Close',
+        acceptButton: 'test Accept',
+      },
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
     };
 
-    const onAccept = () => {
-      console.log('onAccept pressed');
-    };
+    const itemName = 'Hello Testing World';
 
-    const onClose = () => {
-      console.log('onClose pressed');
-    };
     // Act
-    const { asFragment } = render(
-      <ConfirmationDialogComponent
-        isOpen={isOpen}
-        title={title}
-        labels={labels}
-        onAccept={onAccept}
-        onClose={onClose}
-      />
+    render(
+      <ConfirmationDialogComponent {...props}>
+        <Typography variant="body1">{itemName}</Typography>
+      </ConfirmationDialogComponent>
     );
 
-    // Assert
-    expect(asFragment()).toMatchSnapshot();
-  });
-
-  it('should display confirmation dialog using jest-dom', () => {
-    // Arrange
-    const isOpen = true;
-    const title = 'test title';
-    const labels = {
-      closeButton: 'test Close',
-      acceptButton: 'test Accept',
-    };
-    const onAccept = () => {
-      console.log('onAccept pressed');
-    };
-
-    const onClose = () => {
-      console.log('onClose pressed');
-    };
-    // Act
-    const { getByText } = render(
-      <ConfirmationDialogComponent
-        isOpen={isOpen}
-        title={title}
-        labels={labels}
-        onAccept={onAccept}
-        onClose={onClose}
-      />
-    );
+    const element = screen.getByRole('presentation');
 
     // Assert
-    const element = getByText(title);
     expect(element).toBeInTheDocument();
+  });
+
+  it('Should display confirmation dialog components using snapshot testing', () => {
+    // Arrange
+    const props = {
+      isOpen: true,
+      title: 'test title',
+      labels: {
+        closeButton: 'test Close',
+        acceptButton: 'test Accept',
+      },
+      onAccept: jest.fn(),
+      onClose: jest.fn(),
+    };
+
+    const itemName = 'Hello Testing World';
+
+    // Act
+    render(
+      <ConfirmationDialogComponent {...props}>
+        <Typography variant="body1">{itemName}</Typography>
+      </ConfirmationDialogComponent>
+    );
+
+    const element = screen.getByRole('presentation');
+
+    // Assert
+    expect(element).toMatchSnapshot();
   });
 });
